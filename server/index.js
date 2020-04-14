2644a0dd1ceca4b62e4fbba3d4e1a33062503e57
Kris <kriskuiper74@gmail.com>
2020-04-14 15:17:34 +0200

    feat: implement simple spotify authentication flow

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
+require('dotenv-safe').config();
 require('module-alias/register');
 
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const homeRoute = require('@routes/home');
+const spotifyLogin = require('@routes/auth/login');
+const spotifyCallback = require('@routes/auth/callback');
 
-const { DEFAULT_PORT } = require('@lib/constants');
+const { ports } = require('@lib/constants');
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || ports.DEFAULT;
 
 app.use(express.static('client/static'));
+app.use(cookieParser());
+app.use(cors());
 
 app.get('/', homeRoute);
+app.get('/auth/login', spotifyLogin);
+app.get('/auth/callback', spotifyCallback);
 
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
