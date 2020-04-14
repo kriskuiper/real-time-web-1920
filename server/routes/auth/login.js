435c49e217eb9ea1d83f89851009c39cfba8bafc
Kris <kriskuiper74@gmail.com>
2020-04-14 14:57:55 +0200

    feat(login): handle spotify login

---
+++ server/routes/auth/login.js
@@ -0,0 +1,16 @@
+const generateRandomString = require('@lib/generate-random-string');
+const { cookies, spotify } = require('@lib/constants');
+
+const { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } = process.env;
+
+module.exports = (request, response, next) => {
+	const randomString = generateRandomString(11);
+	const spotifyBaseUrl = `${spotify.AUTH_BASEURL}?response_type=code`
+	const scopes = encodeURIComponent('user-read-playback-state user-modify-playback-state');
+	const redirectURI = encodeURIComponent(SPOTIFY_REDIRECT_URI);
+	const	redirectURL = `${spotifyBaseUrl}&client_id=${SPOTIFY_CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectURI}&state=${randomString}`;
+
+	response.cookie(cookies.STATE_KEY, randomString);
+
+	response.redirect(redirectURL);
+}
