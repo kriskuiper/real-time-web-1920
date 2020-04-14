4ea8b0a5b4b6fafe183fe6ee4161a24be6c93962
Kris <kriskuiper74@gmail.com>
2020-04-14 15:16:52 +0200

    feat: modify constants

--- server/lib/constants.js
+++ server/lib/constants.js
@@ -1,1 +1,18 @@
-exports.DEFAULT_PORT = 3000;
+exports.ports = {
+	DEFAULT: 3000
+};
+
+exports.spotify = {
+	AUTH_BASEURL: 'https://accounts.spotify.com/authorize',
+	TOKEN_BASEURL: 'https://accounts.spotify.com/api/token'
+};
+
+exports.cookies = {
+	STATE_KEY: 'stateKey',
+	ACCESS_TOKEN: 'accessToken',
+	REFRESH_TOKEN: 'refreshToken'
+};
+
+exports.errors = {
+	UNAUTHORIZED: 'Sorry, seems like you are unauthorized'
+}
