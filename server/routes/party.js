8c73990a15a8946dcfd38e871ac9136bc12d3bee
Kris <kriskuiper74@gmail.com>
2020-04-21 11:35:38 +0200

    feat(party): check for tokens in cookies

--- server/routes/party.js
+++ server/routes/party.js
@@ -4,6 +4,16 @@ const socket = require('socket.io');
 const activeRooms = {};
 
 module.exports = (request, response) => {
+	const { accessToken, refreshToken } = request.cookies
+
+	if (!accessToken && !refreshToken) {
+		/*
+			@TODO: Let the user know they're not invited to the party
+		*/
+
+		response.redirect('/')
+	}
+
 	const { id } = request.params;
 
 	response.send(renderer.render('pages/party.html', { id }));
