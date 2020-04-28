b8c0b6448cb185cfc98fb37c0cf962b74fa66124
Kris <kriskuiper74@gmail.com>
2020-04-28 11:19:59 +0200

    feat: run a build

--- client/static/scripts/bundle.js
+++ client/static/scripts/bundle.js
@@ -1,1 +1,15 @@
+const elements = {
+	SEARCH_RESULTS: document.getElementById('spotify-search-results'),
+	SEARCH_FORM: document.getElementById('spotify-search'),
+	SEARCH_INPUT: document.getElementById('spotify-search-input')
+};
 
+elements.SEARCH_FORM.addEventListener('submit', async (event) => {
+	event.preventDefault();
+
+	const { value } = elements.SEARCH_INPUT;
+	const response = await fetch(`/api/search?q=${value}`);
+	const json = await response.json();
+
+	console.log(json);
+});
