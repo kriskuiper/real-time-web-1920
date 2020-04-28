811e5af5db584e71872a4daf5cfe004bba662476
Kris <kriskuiper74@gmail.com>
2020-04-28 10:17:36 +0200

    feat(home): delete party related cookies on homepage

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,5 +1,18 @@
 const renderer = require('@lib/renderer');
+const { cookies } = require('@lib/constants');
 
 module.exports = (request, response) => {
+	for (cookie of Object.keys(request.cookies)) {
+		if (isPartyCookie(cookie)) {
+			response.clearCookie(cookie);
+		}
+	}
+
 	response.send(renderer.render('pages/index.html'));
+}
+
+function isPartyCookie(cookie) {
+	return cookie === cookies.PARTY_ID ||
+		cookie === cookies.PARTY_UUID ||
+		cookie === cookies.USER_INTENTION
 }
