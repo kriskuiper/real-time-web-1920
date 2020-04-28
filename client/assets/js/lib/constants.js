3e928391e5fd3eea1af92e031fd85be8c91c11af
Kris <kriskuiper74@gmail.com>
2020-04-28 11:16:48 +0200

    feat(client/constants): add elements

--- client/assets/js/lib/constants.js
+++ client/assets/js/lib/constants.js
@@ -2,3 +2,9 @@ export const cookies = {
 	PARTY_ID: 'partyId',
 	PARTY_UUID: 'partyUUID'
 }
+
+export const elements = {
+	SEARCH_RESULTS: document.getElementById('spotify-search-results'),
+	SEARCH_FORM: document.getElementById('spotify-search'),
+	SEARCH_INPUT: document.getElementById('spotify-search-input')
+}
