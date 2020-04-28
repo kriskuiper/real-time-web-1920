5cad85286c82c19e132f7b3a6284a903b1fba6fe
Kris <kriskuiper74@gmail.com>
2020-04-28 11:16:16 +0200

    feat(client/index.js): search for spotify songs in client

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,1 +1,11 @@
+import { elements } from './lib/constants';
 
+elements.SEARCH_FORM.addEventListener('submit', async (event) => {
+	event.preventDefault();
+
+	const { value } = elements.SEARCH_INPUT;
+	const response = await fetch(`/api/search?q=${value}`);
+	const json = await response.json();
+
+	console.log(json);
+})
