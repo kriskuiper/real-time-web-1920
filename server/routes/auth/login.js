410c82fe4394533c53b782b1a41b75f88613b430
Kris <kriskuiper74@gmail.com>
2020-04-28 11:19:14 +0200

    fix(login): add user-read-private scope

--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -6,7 +6,7 @@ const { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } = process.env;
 module.exports = (request, response, next) => {
 	const randomString = generateRandomString(11);
 	const spotifyBaseUrl = `${spotify.AUTH_BASEURL}?response_type=code`
-	const scopes = encodeURIComponent('user-read-playback-state user-modify-playback-state');
+	const scopes = encodeURIComponent('user-read-playback-state user-modify-playback-state user-read-private');
 	const redirectURI = encodeURIComponent(SPOTIFY_REDIRECT_URI);
 	const	redirectURL = `${spotifyBaseUrl}&client_id=${SPOTIFY_CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectURI}&state=${randomString}`;
 
